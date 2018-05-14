00c9a6930852ab9af67514bf3e1870b8ae2fbedf
Dim Hutsel <dreamtm@yandex.ru>
2018-05-14 16:09:29 +0700

    a comment ensureToken

--- server.js
+++ server.js
@@ -44,13 +44,16 @@ app.post("/api/authentication", jsonParser, function (req, res) {
 
 
 function ensureToken(req, res, next) {
-  var tokenReq = String(req.headers["authorization"]);
-  var tokenSaved = String(fs.readFileSync("./api/token.txt", "utf8"));
-  if (tokenReq === tokenSaved) {
+	
     next();
-  } else {
-    res.sendStatus(403);
-  }
+	
+//  var tokenReq = String(req.headers["authorization"]);
+//  var tokenSaved = String(fs.readFileSync("./api/token.txt", "utf8"));
+//  if (tokenReq === tokenSaved) {
+//    next();
+//  } else {
+//    res.sendStatus(403);
+//  }
 }
 
 //END AUTHENTICATION
